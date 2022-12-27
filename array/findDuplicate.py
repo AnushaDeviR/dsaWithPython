9438ef67746dede81acd8dd95fe44ddddabc0696
AnushaDeviR <anusha3599@gmail.com>
2022-12-27 11:58:07 +0000

    removeDuplicates - array

---
+++ array/findDuplicate.py
@@ -0,0 +1,21 @@
+'''
+Write a function to find the duplicate number on given integer array/list.
+
+Example
+
+removeDuplicates([1, 1, 2, 2, 3, 4, 5])
+Output : [1, 2, 3, 4, 5]
+'''
+
+myList = [1, 1, 2, 2, 3, 4, 5]
+
+
+def removeDuplicates(arr):
+    newArray = []
+    for i in arr:
+        if i not in newArray:
+            newArray.append(i)
+        print(newArray)
+
+
+print(removeDuplicates(myList))
