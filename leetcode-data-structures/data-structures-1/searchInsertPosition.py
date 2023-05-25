583e7a37443035beee935c4103212542fb2a625f
Anushadevi.R <68412863+AnushaDeviR@users.noreply.github.com>
2023-05-25 18:06:17 +0100

    leetcode 35 solution

---
+++ leetcode-data-structures/data-structures-1/searchInsertPosition.py
@@ -0,0 +1,36 @@
+'''
+Given a sorted array of distinct integers and a target value, return the index if the target is found. 
+If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+
+Example 2:
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+
+Example 3:
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+'''
+
+def searchInsert(self, nums: List[int], target: int) -> int:
+
+    l = 0 
+    r = len(nums) - 1
+
+    while l <= r: 
+        mid = (l+r) // 2
+        if nums[mid] == target:
+            print(nums[mid])
+            return mid
+        elif nums[mid] < target:
+            l = mid + 1
+        else: 
+            r = mid - 1
+
+    return l
+                
