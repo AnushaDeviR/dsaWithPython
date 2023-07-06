47286cd3264dc2d3a503e4f95ad5a553b77e5df8
AnushaDeviR <anusha3599@gmail.com>
2023-07-06 23:55:10 +0100

    readme update

--- leetcode-data-structures/data-structures-1/linkedListCycle.py
+++ leetcode-data-structures/data-structures-1/linkedListCycle.py
@@ -1,8 +1,44 @@
+'''
+Given `head`, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the `next` pointer. Internally, `pos` is used to denote the index of the node that tail's `next` pointer is connected to. **Note that `pos` is not passed as a parameter**.
+
+Return `true` *if there is a cycle in the linked list*. Otherwise, return `false`.
+
+**Example 1:**
+
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+
+
+**Example 2:**
+
+Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+
+
+**Example 3:**
+
+Input: head = [1], pos = -1
+Output: false
+Explanation: There is no cycle in the linked list.
+
+
+**Constraints:**
+
+- The number of the nodes in the list is in the range `[0, 104]`.
+- `105 <= Node.val <= 105`
+- `pos` is `1` or a **valid index** in the linked-list.
+'''
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
+
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
@@ -25,4 +61,3 @@ By using Floyd’s algorithm (cycle-finding algorithm) which uses 2 pointers - s
 fast method where slow starts at the 0th index and fast is at slow += 2. 
 So when slow == fast then it means that there is a cycle. When a cycle exists in a LL no matter what slow and fast meets at a same node.
 '''
-
