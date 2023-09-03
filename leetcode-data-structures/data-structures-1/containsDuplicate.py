c96f3402557093f41e7d0b4f10dfe1bbbd266ec6
AnushaDeviR <anusha3599@gmail.com>
2023-09-03 17:40:50 +0100

    added brute force code for leetcode 217

--- leetcode-data-structures/data-structures-1/containsDuplicate.py
+++ leetcode-data-structures/data-structures-1/containsDuplicate.py
@@ -14,8 +14,18 @@ Example 3:
 Input: nums = [1,1,1,3,3,4,3,2,4,2]
 Output: true
 '''
-
+                
 def containsDuplicate(self, nums: List[int]) -> bool:
+    '''
+    The below code takes time complexity of O(n^2) and space complexity of O(1)
+    '''
+
+    # count = 0
+    # for i in range(0, len(nums)):
+    #     for j in range(i+1, len(nums)):
+    #         if nums[i] == nums[j]:
+    #             print(nums[j])
+    #             return True
     
     '''
     set() function returns randomized unordered unique (no elements) 
