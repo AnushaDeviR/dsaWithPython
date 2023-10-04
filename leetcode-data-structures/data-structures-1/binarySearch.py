979e4d0fbd746811a67cfc18c75d79346c613ac5
AnushaDeviR <anusha3599@gmail.com>
2023-10-04 12:23:51 +0100

    alternative findMid formula update at soln 704

--- leetcode-data-structures/data-structures-1/binarySearch.py
+++ leetcode-data-structures/data-structures-1/binarySearch.py
@@ -32,7 +32,7 @@ def search(self, nums: List[int], target: int) -> int:
 
     while l <= r:
 
-        mid = l + (r - l) // 2
+        mid = (r + l) // 2 # or mid = l + (r - l)//2
 
         if nums[mid] < target:
             l = mid + 1
